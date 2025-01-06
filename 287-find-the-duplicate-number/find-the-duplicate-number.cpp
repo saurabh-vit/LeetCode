1a4ae43ce2469e5e465f0d0467e7ee2bec30cf28
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2025-01-06 13:08:58 +0530

    Time: 3 ms (69.85%) | Memory: 65.1 MB (57.00%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -14,13 +14,27 @@ public:
         // return -1;
 
         //another approach
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i<n; i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        // for(int i = 0; i<n; i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        //linked list ans slow fast approach
+        int slow = nums[0], fast = nums[0];
+        do{
+            slow = nums[slow];  //+1
+            fast = nums[nums[fast]]; //+2
+        }while(slow!=fast);
+
+        slow = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];  //+1
+            fast = nums[fast];  //+1
         }
-        return -1;
+        return slow;
     }
 };
