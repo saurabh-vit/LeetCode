80469f80320332f42fd3edc02e3cdd7e59cac917
SAURABH RAJ <113050183+saurabh-vit@users.noreply.github.com>
2025-10-11 00:26:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (48.99%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    void set(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &finalset){
-        if(i == nums.size()){
-            finalset.push_back({ans});
+    void helper(vector<int> &nums, vector<vector<int>> &ans, vector<int> &curr, int i) {
+        if (i == nums.size()) {
+            ans.push_back(curr);
             return;
         }
-        ans.push_back(nums[i]);
-        set(nums, ans, i+1, finalset);
-        ans.pop_back();
-         while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
-            i++;
+        // Include nums[i]
+        curr.push_back(nums[i]);
+        helper(nums, ans, curr, i + 1);
+
+        // Exclude nums[i]
+        curr.pop_back();
+        int idx = i+1;
+        while(idx<nums.size() && nums[i]==nums[idx]){
+            idx++;
         }
-        set(nums, ans, i+1, finalset);
+        helper(nums, ans, curr, idx);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> finalset;
-        vector<int> ans;
         sort(nums.begin(), nums.end());
-        set(nums, ans, 0, finalset);
-        return finalset;
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(nums, ans, curr, 0);
+        return ans;
     }
 };
